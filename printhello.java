c76dc73a8c8343f45f03155c407ba813c32a79a8
vanmathivellaiyan <vanmathivellaiyan1995@gmail.com>
2016-07-22 15:35:41 +0530

    Add files via upload

---
+++ printhello.java
@@ -0,0 +1,12 @@
+package aa;
+
+public class printhello {
+	public static void main(String[] args){
+		String str=" hello";
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("hello");
+		}
+	}
+
+}
